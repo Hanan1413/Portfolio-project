a8fb528864a3093ee5c2f358f8a32f1e39f7fef2
Hanan <xxh1165@gmail.com>
2022-03-15 13:40:32 +0300

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+// import Navbar from "./components/Navbar/Navbar"
+
+//  import Home from "./Home";
+// import Contact from "./Contact";
+// import About from "./About"
 import './App.css';
 
-function App() {
+
+ function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to my website</h1>
     </div>
   );
-}
 
+ }
+ 
+ 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Home.css";
+// import img from "."
+
+// const BackgroundHead = {
+//   backgroundImage: 'url('+ BackgroundHeader+')'
+//   }
+
+
+
+ function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default Home;
